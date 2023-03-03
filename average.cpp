40c0e8781a560595b7fd71f454e1ab5cd1f70807
napapon-wongkom <dot.wongkom@gmail.com>
2023-03-03 16:25:59 +0700

    average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
-
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+using namespace std;
+int main(int argc,char *argv[]){
+    float number,sum=0,average;
+    int num_count;
+    if(argc >1){
+        for(int i =0;i < argc ; i++){
+            number = atof(argv[i]);
+            sum+=number;
+        }
+        num_count = argc-1;
+        average = sum/num_count;
+        cout<<"---------------------------------\n";
+        cout<<"Average of "<<num_count<<" numbers = "<<average<<"\n";
+        cout<<"---------------------------------\n";
+    }else{
+        cout<<"Please input numbers to find average.\n";
+    }
+    return 0;
+}
